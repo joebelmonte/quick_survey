7401422801581be4222986905333a3e503155fc4
joebelmonte <joseph.belmonte@gmail.com>
2017-08-30 07:53:33 -0400

    Starting working on Mailer helper

--- config/prod.js
+++ config/prod.js
@@ -4,5 +4,6 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
   stripePublishableKey: process.env.STRIPEPUBLISHABLEKEY,
-  stripeSecretKey: process.env.STRIPESECRETKEY
+  stripeSecretKey: process.env.STRIPESECRETKEY,
+  sendGridKey: process.env.SEND_GRID_KEY
 }
