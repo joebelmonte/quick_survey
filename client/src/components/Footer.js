79d82de5203424da461b33058940e4d4c2726796
joebelmonte <joseph.belmonte@gmail.com>
2018-04-12 20:54:35 -0400

    Fixed typo in footer and project name in payment modal

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -8,7 +8,7 @@ export default function() {
           <div className="col l6 s12">
             <h5 className="white-text">Quick Survey</h5>
             <p className="grey-text text-lighten-4">
-              A demonstration app by Joe Bemonte
+              A demonstration app by Joe Belmonte
             </p>
           </div>
           <div className="col l4 offset-l2 s12">
